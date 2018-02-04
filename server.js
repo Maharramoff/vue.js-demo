402ff0d555b3b12172237b7122684a46b7739b53
meherremoff <sh.maharramov@gmail.com>
2018-02-04 17:14:59 +0400

    codeship to herokuapp history mode problem

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 /**
  * Created by ADMIN on 04.02.2018.
  */
+var history = require('connect-history-api-fallback');
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
+
 let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(history());
+//app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
