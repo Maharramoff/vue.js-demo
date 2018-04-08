3cc2ef1ed6356ab2dc2afd79c5666e193d3cc9cc
meherremoff <sh.maharramov@gmail.com>
2018-04-08 19:44:15 +0400

    unnecessary lines deleted

--- src/store/state.js
+++ src/store/state.js
@@ -2,11 +2,8 @@
  * Created by Maharramoff on 04.02.2018.
  */
 export const state = {
-    appTitle: 'Kod paylaşımı proqramı',
     user    : null,
     error   : null,
     loading : false,
-    appLang : null,
-    welcomeTitle : 'Kod paylaşımı proqramına xoş gəlmisiniz',
-    madeWith : 'Layihə Vue.js-lə hazırlanıb',
+    appLang : null
 }
